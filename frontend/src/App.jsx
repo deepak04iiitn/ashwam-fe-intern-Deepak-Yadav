c5ebae6912301a228a098b846123f9ea717dc04a
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-12-22 11:34:17 +0530

    feat: set up the react router dom in the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Meals from './pages/Meals'
 
 export default function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex-grow">
+          <Routes>
+            <Route path='/' element={<Meals />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
